e3018de8604ac62b2fc87e8f5ad1115973f69136
JohnCharlieAgit#git$git config --global user.name JohnCharlieA <JAtkins@spartaglobal.com>
2022-10-21 15:50:51 +0100

    start and bubble

---
+++ src/main/java/com/sparta/ja/App.java
@@ -0,0 +1,11 @@
+package com.sparta.ja;
+public class App
+{
+    public static void main( String[] args )
+    {
+        Starter starter = new Starter();
+        starter.start();
+
+
+    }
+}
---
+++ src/main/java/com/sparta/ja/SortingFactory.java
@@ -0,0 +1,11 @@
+package com.sparta.ja;
+
+public class SortingFactory {
+    public static boolean sorter(String answer)
+    {
+        switch (answer)
+        {
+            case "bubble":
+        }
+    }
+}
